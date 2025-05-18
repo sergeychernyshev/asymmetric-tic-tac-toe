9d059ac7af7127f42fe35e7e9e6528540c9ba1b1
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2025-05-18 13:09:44 -0400

    Server is here!

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export default {
+  /**
+   * This is the standard fetch handler for a Cloudflare Worker
+   *
+   * @param request - The request submitted to the Worker from the client
+   * @param env - The interface to reference bindings declared in wrangler.jsonc
+   * @param ctx - The execution context of the Worker
+   * @returns The response to be sent back to the client
+   */
+  async fetch(request, env, ctx): Promise<Response> {
+    return new Response('Hello, Tic-Tac-Toe!');
+  },
+};
