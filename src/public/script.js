87a53728ad58eb634067eb680e98886bea795ee7
rjfuhrman42 <reidjf77@gmail.com>
2025-05-18 14:17:47 -0400

    board: setup the x and ohs

--- src/public/script.js
+++ src/public/script.js
@@ -182,3 +182,100 @@ function join() {
   });
 }
 join();
+const game = {
+  xTurn: true,
+  xState: [],
+  oState: [],
+  winningStates: [
+    // Rows
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+
+    // Columns
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+
+    // Diagonal
+    ['0', '4', '8'],
+    ['2', '4', '6'],
+  ],
+};
+
+const squares = document.querySelectorAll('.square');
+const currentTurn = 'x';
+
+squares.forEach((square) => {
+  console.log(square.classList);
+  square.addEventListener('click', (e) => {
+    if (square.classList.contains('o') || square.classList.contains('x')) {
+      return;
+    }
+
+    square.classList.add(currentTurn);
+  });
+});
+
+// const socketIndicator = document.querySelector('#socket');
+// let currentWebSocket = null;
+// function join() {
+//   // If we are running via wrangler dev, use ws:
+//   const wss = document.location.protocol === 'http:' ? 'ws://' : 'wss://';
+//   let ws = new WebSocket(`${wss}${window.location.hostname}:${window.location.port}/websocket`);
+//   let rejoined = false;
+//   let startTime = Date.now();
+
+//   let rejoin = async () => {
+//     if (!rejoined) {
+//       rejoined = true;
+//       currentWebSocket = null;
+//       setConnectionIndicator();
+
+//       // Don't try to reconnect too rapidly.
+//       let timeSinceLastJoin = Date.now() - startTime;
+//       if (timeSinceLastJoin < 10000) {
+//         // Less than 10 seconds elapsed since last join. Pause a bit.
+//         await new Promise((resolve) => setTimeout(resolve, 10000 - timeSinceLastJoin));
+//       }
+
+//       // OK, reconnect now!
+//       join();
+//     }
+//   };
+
+//   ws.addEventListener('open', (event) => {
+//     currentWebSocket = ws;
+//     setConnectionIndicator();
+
+//     // Send user info message.
+//     ws.send(JSON.stringify({ connected: true }));
+//   });
+
+//   ws.addEventListener('message', (event) => {
+//     let message = JSON.parse(event.data);
+//     // console.log('Received message: ', message);
+
+//     if (message.message) {
+//       chat.push(message.message);
+//       renderChat();
+//     }
+
+//     if (message.chat) {
+//       chat = message.chat;
+//       renderChat();
+//     }
+//   });
+
+//   ws.addEventListener('close', (event) => {
+//     console.log('WebSocket closed, reconnecting:', event.code, event.reason);
+
+//     rejoin();
+//   });
+
+//   ws.addEventListener('error', (event) => {
+//     console.log('WebSocket error, reconnecting:', event);
+//     rejoin();
+//   });
+// }
+// join();
