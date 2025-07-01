41d78e9db24d5b2cf17646f902dfc84d936d8117
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2025-07-01 11:07:01 -0400

    Enabling it back

--- .gitterra.config.mjs
+++ .gitterra.config.mjs
@@ -1,6 +1,6 @@
 export default (config) => {
   config.timelapseLookBackPerfRun = 1000;
-  config.createTimelapse = false;
+  config.createTimelapse = true;
   config.minTiles = 1;
 
   return config;
