729dc57cd1d6901a9e6af839f240b0383237d75e
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2025-07-01 11:06:44 -0400

    Hmm. Resetting the timelapse.

--- .gitterra.config.mjs
+++ .gitterra.config.mjs
@@ -1,6 +1,6 @@
 export default (config) => {
   config.timelapseLookBackPerfRun = 1000;
-  config.createTimelapse = true;
+  config.createTimelapse = false;
   config.minTiles = 1;
 
   return config;
